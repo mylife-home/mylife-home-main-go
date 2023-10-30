0c9531f20bf2283cdcc299a63d2fcc165180f1e5
vincent-tr <vincent.tr@gmail.com>
2023-10-30 14:56:56 +0100

    executor timer

--- mylife-home-common/executor/helpers.go
+++ mylife-home-common/executor/helpers.go
@@ -34,3 +34,31 @@ func NewTicker(duration time.Duration, callback func()) *Ticker {
 func (ticker *Ticker) Stop() {
 	ticker.cancel()
 }
+
+type Timer struct {
+	cancel func()
+}
+
+func NewTimer(duration time.Duration, callback func()) *Timer {
+	exec := CreateExecutor()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		defer exec.Terminate()
+
+		select {
+		case <-time.After(duration):
+			exec.Execute(callback)
+		case <-Context().Done():
+			return
+		case <-ctx.Done():
+			return
+		}
+	}()
+
+	return &Timer{cancel}
+}
+
+func (timer *Timer) Stop() {
+	timer.cancel()
+}
