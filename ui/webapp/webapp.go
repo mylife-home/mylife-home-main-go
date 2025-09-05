3c3c6f940837a8653bfb661bb6ee900adbd1bc9b
vincent-tr <vincent.tr@gmail.com>
2025-09-05 17:57:51 +0200

    clean embed

--- ui/webapp/webapp.go
+++ ui/webapp/webapp.go
@@ -2,19 +2,7 @@ package webapp
 
 import (
 	"embed"
-	"io/fs"
 )
 
 //go:embed all:dist
-var build embed.FS
-
-var FS fs.FS
-
-func init() {
-	var err error
-	FS, err = fs.Sub(build, "dist/prod/static")
-
-	if err != nil {
-		panic(err)
-	}
-}
+var FS embed.FS
