b50161af663ad1345ab7a222fa9c1dfdc54d4527
vincent-tr <vincent.tr@gmail.com>
2025-09-12 19:15:44 +0200

    state manager

--- ui/mylife-home-ui/pkg/web/sessions/state.go
+++ ui/mylife-home-ui/pkg/web/sessions/state.go
@@ -1,7 +1,6 @@
 package sessions
 
 import (
-	"fmt"
 	"mylife-home-common/components"
 	"mylife-home-common/components/metadata"
 	"mylife-home-common/tools"
@@ -12,7 +11,8 @@ type stateListener struct {
 	registry            components.Registry
 	componentChangeChan chan *components.ComponentChange
 
-	state    map[string]any
+	onChange func(componentId string, stateName string, value any)
+	state    map[string]map[string]any
 	stateMux sync.Mutex
 
 	subscriptions map[string]subscription
@@ -23,11 +23,12 @@ type subscription struct {
 	ch    chan any
 }
 
-func newStateListener(registry components.Registry) *stateListener {
+func newStateListener(registry components.Registry, onChange func(componentId string, stateName string, value any)) *stateListener {
 	l := &stateListener{
 		registry:            registry,
 		componentChangeChan: make(chan *components.ComponentChange),
-		state:               make(map[string]any),
+		onChange:            onChange,
+		state:               make(map[string]map[string]any),
 		subscriptions:       make(map[string]subscription),
 	}
 
@@ -41,6 +42,22 @@ func newStateListener(registry components.Registry) *stateListener {
 func (l *stateListener) Terminate() {
 	l.registry.OnComponentChange().Unsubscribe(l.componentChangeChan)
 	close(l.componentChangeChan)
+}
+
+// nil if component not present
+func (l *stateListener) GetState(componentId string) map[string]any {
+	l.stateMux.Lock()
+	defer l.stateMux.Unlock()
+
+	stateCopy := make(map[string]any)
+	if compState, ok := l.state[componentId]; ok {
+		for k, v := range compState {
+			stateCopy[k] = v
+		}
+		return stateCopy
+	}
+
+	return nil
 }
 
 type stateChange struct {
@@ -84,41 +101,55 @@ func (l *stateListener) handleComponentChange(change *components.ComponentChange
 		return
 	}
 
-	stateNames := l.getStateNames(comp)
-
 	switch change.Action() {
 	case components.RegistryAdd:
-		for _, name := range stateNames {
-			key := comp.Id() + ":" + name
-			value := comp.StateItem(name)
-
-			sub := subscription{
-				value: value,
-				ch:    make(chan any, 10), // else subscribe current value below will deadlock
-			}
-
-			sub.value.Subscribe(sub.ch, true)
-			l.subscriptions[key] = sub
-
-			merger.Add(tools.MapChannel(sub.ch, func(v any) stateChange {
-				return stateChange{
-					componentId: comp.Id(),
-					stateName:   name,
-					value:       v,
-				}
-			}))
-		}
+		l.handleComponentAdd(comp, merger)
 
 	case components.RegistryRemove:
-		for _, name := range stateNames {
-			key := comp.Id() + ":" + name
-			sub := l.subscriptions[key]
-			delete(l.subscriptions, key)
-
-			sub.value.Unsubscribe(sub.ch)
-			close(sub.ch)
-		}
+		l.handleComponentRemove(comp)
 	}
+}
+
+func (l *stateListener) handleComponentAdd(comp components.Component, merger *tools.ChannelMerger[stateChange]) {
+	for _, name := range l.getStateNames(comp) {
+		key := comp.Id() + ":" + name
+		value := comp.StateItem(name)
+
+		sub := subscription{
+			value: value,
+			ch:    make(chan any, 10), // else subscribe current value below will deadlock
+		}
+
+		sub.value.Subscribe(sub.ch, true)
+		l.subscriptions[key] = sub
+
+		merger.Add(tools.MapChannel(sub.ch, func(v any) stateChange {
+			return stateChange{
+				componentId: comp.Id(),
+				stateName:   name,
+				value:       v,
+			}
+		}))
+	}
+
+	l.stateMux.Lock()
+	l.state[comp.Id()] = make(map[string]any)
+	l.stateMux.Unlock()
+}
+
+func (l *stateListener) handleComponentRemove(comp components.Component) {
+	for _, name := range l.getStateNames(comp) {
+		key := comp.Id() + ":" + name
+		sub := l.subscriptions[key]
+		delete(l.subscriptions, key)
+
+		sub.value.Unsubscribe(sub.ch)
+		close(sub.ch)
+	}
+
+	l.stateMux.Lock()
+	l.state[comp.Id()] = make(map[string]any)
+	l.stateMux.Unlock()
 }
 
 func (l *stateListener) getStateNames(comp components.Component) []string {
@@ -137,10 +168,19 @@ func (l *stateListener) getStateNames(comp components.Component) []string {
 }
 
 func (l *stateListener) handleStateChange(change stateChange) {
+	l.updateState(change)
+	l.onChange(change.componentId, change.stateName, change.value)
+}
+
+func (l *stateListener) updateState(change stateChange) {
 	l.stateMux.Lock()
 	defer l.stateMux.Unlock()
 
-	key := change.componentId + ":" + change.stateName
-	l.state[key] = change.value
-	fmt.Println("State updated:", key, "=", change.value)
+	compState := l.state[change.componentId]
+	if compState == nil {
+		// Late notification for a component that has been removed
+		return
+	}
+
+	compState[change.stateName] = change.value
 }
